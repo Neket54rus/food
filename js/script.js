11854ae8146df7177986ee17508ceb794eb57df0
‘Nikita’ <kvar228@mail.ru>
2022-03-15 13:55:00 +0700

    refactoring timer

--- js/script.js
+++ js/script.js
@@ -102,36 +102,99 @@ window.addEventListener("DOMContentLoaded", () => {
 	}, 2000) //интервал 2 сек
 
 	//Создание таймера
-	const endDate = new Date("2022-05-20") //устанавливаем конечную дату
-	const endDateMls = endDate.getTime() //переводим конечную дату в мил/сек
-	const days = document.querySelector("#days") //получаем элемент с днем
-	const hours = document.querySelector("#hours") //получаем элемент с часами
-	const minutes = document.querySelector("#minutes") //получаем элемент с минутами
-	const seconds = document.querySelector("#seconds") //получаем элемент с секундами
+	//таймер Никита
+	// const endDate = new Date("2022-05-20") //устанавливаем конечную дату
+	// const endDateMls = endDate.getTime() //переводим конечную дату в мил/сек
+	// const days = document.querySelector("#days") //получаем элемент с днем
+	// const hours = document.querySelector("#hours") //получаем элемент с часами
+	// const minutes = document.querySelector("#minutes") //получаем элемент с минутами
+	// const seconds = document.querySelector("#seconds") //получаем элемент с секундами
 
-	//Функция по установки нуля
-	function setZero(num) {
+	// //Функция по установки нуля
+	// function setZero(num) {
+	// 	//функция принимает в себя цифру
+	// 	//условие: если число будет меньше 10, то функция возвращает строку с нулем и нашем числом, если нет, то возвращает просто число
+	// 	if (num < 10) {
+	// 		return `0${num}`
+	// 	} else {
+	// 		return num
+	// 	}
+	// }
+
+	// //создаем интервал обновления таймера
+	// setInterval(() => {
+	// 	let date = new Date() //получаем наше время
+	// 	let dateMlsec = date.getTime() //переводим наше время в мил/сек
+	// 	let sec = Math.floor(((endDateMls - dateMlsec) / 1000) % 60) //получаем секунды
+	// 	let min = Math.floor(((endDateMls - dateMlsec) / 1000 / 60) % 60) //получаем минуты
+	// 	let hour = Math.floor(((endDateMls - dateMlsec) / 1000 / 60 / 60) % 24) //получаем часы
+	// 	let day = Math.floor((endDateMls - dateMlsec) / 1000 / 60 / 60 / 24) //получаем дни
+
+	// 	days.textContent = setZero(day) //вставляем нове значение дня в элемент с днем
+	// 	hours.textContent = setZero(hour) //вставляем нове значение часа в элемент с часами
+	// 	minutes.textContent = setZero(min) //вставляем нове значение минут в элемент с минутами
+	// 	seconds.textContent = setZero(sec) //вставляем нове значение секунд в элемент с секундами
+	// }, 1000) //обновление каждую секунду
+
+	//таймер курс
+	const deadline = "2022-05-20" //создаем конечную дату
+
+	//функция по получению оставшегося времени
+	//функция принимает в себя конечную дату
+	function getTimeRemaining(endtime) {
+		const t = Date.parse(endtime) - Date.parse(new Date()), //получаем разницу между конечной даты и нашей даты
+			days = Math.floor(t / (1000 * 60 * 60 * 24)), //переводим в дни
+			hours = Math.floor((t / (1000 * 60 * 60)) % 24), //переводим в часы
+			minutes = Math.floor((t / 1000 / 60) % 60), //переводим в минуты
+			seconds = Math.floor((t / 1000) % 60) //переводим в секунды
+
+		//выводим массив с нашими числами
+		return {
+			total: t,
+			days: days,
+			hours: hours,
+			minutes: minutes,
+			seconds: seconds,
+		}
+	}
+
+	//функци по установки нуля
+	function getZero(num) {
 		//функция принимает в себя цифру
 		//условие: если число будет меньше 10, то функция возвращает строку с нулем и нашем числом, если нет, то возвращает просто число
-		if (num < 10) {
+		if (num >= 0 && num < 10) {
 			return `0${num}`
 		} else {
 			return num
 		}
 	}
 
-	//создаем интервал обновления таймера
-	setInterval(() => {
-		let date = new Date() //получаем наше время
-		let dateMlsec = date.getTime() //переводим наше время в мил/сек
-		let sec = Math.floor(((endDateMls - dateMlsec) / 1000) % 60) //получаем секунды
-		let min = Math.floor(((endDateMls - dateMlsec) / 1000 / 60) % 60) //получаем минуты
-		let hour = Math.floor(((endDateMls - dateMlsec) / 1000 / 60 / 60) % 24) //получаем часы
-		let day = Math.floor((endDateMls - dateMlsec) / 1000 / 60 / 60 / 24) //получаем дни
+	//функция по установки таймера
+	//функция принимает в себя селектор родителя и конечной даты
+	function setClock(selector, endTime) {
+		const timer = document.querySelector(selector), //получаем родителя со страницы
+			days = timer.querySelector("#days"), //получаем день со страницы
+			hours = timer.querySelector("#hours"), //получаем часы со страницы
+			minutes = timer.querySelector("#minutes"), //получаем минуты со страницы
+			seconds = timer.querySelector("#seconds"), //получаем секунды со страницы
+			timeInterval = setInterval(updateClock, 1000) //устанавливаем обновление таймера на странице каждую секунду
 
-		days.textContent = setZero(day) //вставляем нове значение дня в элемент с днем
-		hours.textContent = setZero(hour) //вставляем нове значение часа в элемент с часами
-		minutes.textContent = setZero(min) //вставляем нове значение минут в элемент с минутами
-		seconds.textContent = setZero(sec) //вставляем нове значение секунд в элемент с секундами
-	}, 1000) //обновление каждую секунду
+		updateClock() //запускаем функцию по обновлению таймера
+
+		//функция по обновлению таймера
+		function updateClock() {
+			const t = getTimeRemaining(endTime) // принимаем с числами из функции getTimeRemaining
+
+			days.textContent = getZero(t.days) //обновляем на страницы дни
+			hours.textContent = getZero(t.hours) //обновляем на страницы часы
+			minutes.textContent = getZero(t.minutes) //обновляем на страницы минуты
+			seconds.textContent = getZero(t.seconds) //обновляем на страницы секунды
+
+			//останавливаем интервал, если время равно 0
+			if (t.total <= 0) {
+				clearInterval(timeInterval)
+			}
+		}
+	}
+	setClock(".timer", deadline) //запуск таймера
 })
