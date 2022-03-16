d6f412300bc5c263ed186e95f8742a1cc35a9b9e
‘Nikita’ <kvar228@mail.ru>
2022-03-16 09:32:35 +0700

    refactor modal window

--- js/script.js
+++ js/script.js
@@ -197,24 +197,52 @@ window.addEventListener("DOMContentLoaded", () => {
 		}
 	}
 	setClock(".timer", deadline) //запуск таймера
-})
 
-//модальное окно
-const modalWindow = document.querySelector(".modal") //получаем модальное окно со страницы
-const modalBtn = document.querySelectorAll("[data-modal]") //получаем кнопки для открытия модального окна
+	//модальное окно
+	const modalWindow = document.querySelector(".modal") //получаем модальное окно со страницы
+	const modalBtn = document.querySelectorAll("[data-modal]") //получаем кнопки для открытия модального окна
+	const modalCloseBtn = document.querySelector(".modal__close") //получаем кнопку для закрытия модалки
 
-//перебираем все кнопки выводим каждую отдельно
-modalBtn.forEach((btn) => {
-	//на каждую кнопку навешиваем клик
-	btn.addEventListener("click", () => {
-		modalWindow.style.display = "block" //устанавилваем модальному окну display в block тем самым показываем модальное окно
+	//перебираем все кнопки выводим каждую отдельно
+	modalBtn.forEach((btn) => {
+		//на каждую кнопку навешиваем клик
+		btn.addEventListener("click", () => {
+			modalWindow.style.display = "block" //устанавилваем модальному окну display в block тем самым показываем модальное окно
+			document.body.style.overflow = "hidden" //убираем возможность прокрутки
+			clearInterval(modalTimer) //отключаем показ модального окна через время, если пользователь сам открыл его
+		})
 	})
-})
 
-//навешиваем обработчик клика на модальное окно
-modalWindow.addEventListener("click", (e) => {
-	//условие: если элемент на который мы нажали имеет класс modal или modal__close, то закрываем модальное окно
-	if (e.target.classList.contains("modal") || e.target.classList.contains("modal__close")) {
-		modalWindow.style.display = "none"
+	//навешиваем обработчик клика на модальное окно
+	modalWindow.addEventListener("click", (e) => {
+		//условие: если элемент на который мы нажали имеет класс modal или modal__close, то закрываем модальное окно
+		if (e.target == modalWindow || e.target == modalCloseBtn) {
+			modalWindow.style.display = "none" //показываем модальное окно
+			document.body.style.overflow = "" //включаем возможность прокрутки
+		}
+	})
+
+	//убираем модальное окна при нажатии на esc
+	document.addEventListener("keydown", (e) => {
+		if (e.code === "Escape" && window.getComputedStyle(modalWindow).display == "block") {
+			modalWindow.style.display = "none" //показываем модальное окно
+			document.body.style.overflow = "" //включаем возможность прокрутки
+		}
+	})
+
+	//Установка интервала на открытие модального окна
+	const modalTimer = setInterval(() => {
+		modalWindow.style.display = "block" //устанавилваем модальному окну display в block тем самым показываем модальное окно
+		document.body.style.overflow = "hidden" //убираем возможность прокрутки
+	}, 50000)
+
+	function showModalByScroll() {
+		if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+			modalWindow.style.display = "block" //устанавилваем модальному окну display в block тем самым показываем модальное окно
+			document.body.style.overflow = "hidden" //убираем возможность прокрутки
+			window.removeEventListener("scroll", showModalByScroll)
+		}
 	}
+
+	window.addEventListener("scroll", showModalByScroll)
 })
